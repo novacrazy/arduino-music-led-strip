7808528645db1a30745fc35e520cb307c6438eb7
novacrazy <novacrazy@gmail.com>
2016-01-01 06:04:15 -0600

    Add main command protocol testing code

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,34 +1,69 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
-#include <ArduinoThread/Thread.h>
-#include <ArduinoThread/ThreadController.h>
 
-#include <FastLED/FastLED.h>
+#include "pin_config.h"
 
-#define NUM_LEDS 144
-#define LED_STRIP_PIN  16
+#include "led_strip.h"
 
-int LCD_RS = 52,
-    LCD_ENABLE = 53;
+#include "command_protocol.h"
 
-int LCD_D[4] = {48, 49, 50, 51};
+#include "commands.h"
 
-LiquidCrystal* lcd;
+LiquidCrystal *lcd;
+CommandProtocol *protocol;
 
+void commandCallback(Command *cmd) {
+    switch (cmd->getCommand()) {
+        case ECHO_COMMAND: {
+            Serial.write(cmd->getData(), cmd->getSize());
+            break;
+        }
+        default: {
+            Serial.print(F("Unknown Command"));
+        }
+    }
+}
 
 void setup() {
-    pinMode(13, OUTPUT);
+    Serial.begin(9600);
 
-    lcd = new LiquidCrystal(LCD_RS, LCD_ENABLE, LCD_D[0], LCD_D[1], LCD_D[2], LCD_D[3]);
+    while (!Serial) {
+        delay(100);
+    }
+
+    //pinMode(13, OUTPUT);
+
+    lcd = new LiquidCrystal(LCD_PINS_ALL);
 
     lcd->begin(20, 4);
     lcd->clear();
-    lcd->print("Ready for input");
+    lcd->print(F("Ready for input"));
+
+    //initialize_led_strip();
+
+    //FastLED.setDither(BINARY_DITHER);
+    //FastLED.setDither(DISABLE_DITHER);
+    //FastLED.setBrightness(25);
+
+    protocol = new CommandProtocol();
+
+    protocol->start();
+
+    Serial.print(F("Initialized"));
 }
 
+static Command request_command(REQUEST_ACTION_COMMAND);
+
 void loop() {
-    digitalWrite(13, 1);
-    delay(500);
-    digitalWrite(13, 0);
-    delay(500);
+    protocol->yield();
+
+    if (!protocol->isWaiting()) {
+        bool res = protocol->sendRequest(&request_command, &commandCallback);
+
+        if (!res) {
+            Serial.print("Error");
+        }
+    }
+
+    delay(10);
 }
