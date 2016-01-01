13bfdef664ce783aecade2d757c7f23ceb704f47
novacrazy <novacrazy@gmail.com>
2016-01-01 05:12:00 -0600

    Add pin config

---
+++ controller/pin_config.h
@@ -0,0 +1,25 @@
+//
+// Created by Aaron on 12/31/2015.
+//
+
+#ifndef CONTROLLER_PIN_CONFIG_H
+#define CONTROLLER_PIN_CONFIG_H
+
+#define NUM_LEDS 144
+#define LED_DATA_PIN  16
+
+#define LCD_RS 52
+#define LCD_ENABLE 53
+
+#define LCD_PIN_4 48
+#define LCD_PIN_5 49
+#define LCD_PIN_6 50
+#define LCD_PIN_7 51
+
+#define LCD_PINS_ALL LCD_RS, LCD_ENABLE, LCD_PIN_4, LCD_PIN_5, LCD_PIN_6, LCD_PIN_7
+
+//LEDs for stereo channel display
+#define LEFT_LED_PIN 5
+#define RIGHT_LED_PIN 6
+
+#endif //CONTROLLER_PIN_CONFIG_H
