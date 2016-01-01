cffb91c335ca90f64c3edb2a941a8803ad978ed1
novacrazy <novacrazy@gmail.com>
2016-01-01 05:54:43 -0600

    Add some basic command values

---
+++ controller/commands.h
@@ -0,0 +1,19 @@
+//
+// Created by Aaron on 12/31/2015.
+//
+
+#ifndef CONTROLLER_COMMANDS_H
+#define CONTROLLER_COMMANDS_H
+
+#define REQUEST_ACTION_COMMAND          0x0
+
+#define ECHO_COMMAND                    0x1
+#define WRITE_LCD_COMMAND               0x2
+#define CLEAR_LCD_COMMAND               0x3
+#define WRITE_STEREO_COMMAND            0x4
+#define ENTER_LOW_POWER_MODE_COMMAND    0x5
+#define CHANGE_COLOR_TEMPERATURE        0x6
+#define CHANGE_COLOR_CORRECTION         0x7
+#define CHANGE_LED_BRIGHTNESS           0x8
+
+#endif //CONTROLLER_COMMANDS_H
