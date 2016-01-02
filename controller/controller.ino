be973156882aea57bb118e6ef9e28b4729bfa3fe
novacrazy <novacrazy@gmail.com>
2016-01-02 03:21:25 -0600

    Commit working implementation of the LED strip controller with one visualization

--- controller/controller.ino
+++ controller/controller.ino
@@ -3,14 +3,21 @@
 
 #include "pin_config.h"
 
-#include "led_strip.h"
-
 #include "command_protocol.h"
 
 #include "commands.h"
 
+#include "led_strip.h"
+
 LiquidCrystal *lcd;
 CommandProtocol *protocol;
+float channels[3];
+
+static Command request_command(COMMAND_REQUEST_ACTION);
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 void commandCallback(Command *cmd) {
     switch (cmd->getCommand()) {
@@ -26,52 +33,102 @@ void commandCallback(Command *cmd) {
             //Discard
             break;
         }
+        case COMMAND_WRITE_STEREO: {
+            memcpy(channels, cmd->getData(), sizeof(float) * 3);
+
+            float max_recorded = channels[2];
+
+            analogWrite(LEFT_LED_PIN, mapf(channels[0], 0, max_recorded, 0, 255));
+            analogWrite(RIGHT_LED_PIN, mapf(channels[1], 0, max_recorded, 0, 255));
+
+            break;
+        }
         default: {
+            digitalWrite(13, HIGH);
+
             Serial.print(F("Unknown Command"));
         }
     }
 }
 
 void setup() {
-    Serial.begin(9600);
+    delay(500); //for power overload protection
+
+    Serial.begin(256000, SERIAL_8E2);
 
     while (!Serial) {
         delay(100);
     }
 
-    //pinMode(13, OUTPUT);
+    protocol = new CommandProtocol();
+
+    protocol->start(commandCallback);
+
+    initialize_led_strip();
+
+    FastLED.setDither(BINARY_DITHER);
+    //FastLED.setDither(DISABLE_DITHER);
+    FastLED.setBrightness(30);
 
     lcd = new LiquidCrystal(LCD_PINS_ALL);
 
     lcd->begin(20, 4);
     lcd->clear();
-    lcd->print(F("Ready for input"));
-
-    //initialize_led_strip();
-
-    //FastLED.setDither(BINARY_DITHER);
-    //FastLED.setDither(DISABLE_DITHER);
-    //FastLED.setBrightness(25);
-
-    protocol = new CommandProtocol();
-
-    protocol->start();
 
     Serial.print(F("Initialized"));
 }
 
-static Command request_command(COMMAND_REQUEST_ACTION);
+void showLEDs() {
+    int channel_count = NUM_LEDS / 2;
+
+    float whole, max_recorded = channels[2];
+
+    float left_max = mapf(channels[0], 0, max_recorded, 0, channel_count);
+    float right_max = NUM_LEDS - mapf(channels[1], 0, max_recorded, 0, channel_count);
+
+    CRGB color = CRGB::Blue;
+    CRGB black = CRGB::Black;
+
+    for (int i = 0; i < channel_count; i++) {
+        if (left_max > i) {
+            LED_STRIP[i] = color;
+
+        } else {
+            LED_STRIP[i] = black;
+        }
+    }
+
+    for (int i = 0; i < NUM_LEDS - 2; i++) {
+        LED_STRIP[i + 1] = LED_STRIP[i].lerp8(LED_STRIP[i + 1], 127);
+    }
+
+    for (int i = NUM_LEDS - 1; i >= channel_count; i--) {
+        if (right_max < i) {
+            LED_STRIP[i] = color;
+
+        } else {
+            LED_STRIP[i] = black;
+        }
+    }
+
+    for (int i = NUM_LEDS - 1; i > channel_count; i--) {
+        LED_STRIP[i - 1] = LED_STRIP[i].lerp8(LED_STRIP[i - 1], 127);
+    }
+
+
+    FastLED.delay(10);
+}
 
 void loop() {
     protocol->run();
 
     if (!protocol->isWaiting()) {
-        bool res = protocol->sendCommand(request_command, commandCallback);
+        showLEDs();
+
+        bool res = protocol->sendCommand(request_command);
 
         if (!res) {
-            Serial.print("Error");
+            Serial.write("Error");
         }
     }
-
-    delay(10);
 }
