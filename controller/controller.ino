d9c435e72555ab076f4157588490f09e208b90a1
novacrazy <novacrazy@gmail.com>
2016-01-02 04:06:24 -0600

    Use a blended rainbow for amplitude visualization

--- controller/controller.ino
+++ controller/controller.ino
@@ -36,11 +36,6 @@ void commandCallback(Command *cmd) {
         case COMMAND_WRITE_STEREO: {
             memcpy(channels, cmd->getData(), sizeof(float) * 3);
 
-            float max_recorded = channels[2];
-
-            analogWrite(LEFT_LED_PIN, mapf(channels[0], 0, max_recorded, 0, 255));
-            analogWrite(RIGHT_LED_PIN, mapf(channels[1], 0, max_recorded, 0, 255));
-
             break;
         }
         default: {
@@ -86,35 +81,24 @@ void showLEDs() {
     float left_max = mapf(channels[0], 0, max_recorded, 0, channel_count);
     float right_max = NUM_LEDS - mapf(channels[1], 0, max_recorded, 0, channel_count);
 
-    CRGB color = CRGB::Blue;
-    CRGB black = CRGB::Black;
+    fill_rainbow(LED_STRIP, NUM_LEDS, 0, 255 / NUM_LEDS);
 
     for (int i = 0; i < channel_count; i++) {
-        if (left_max > i) {
-            LED_STRIP[i] = color;
-
-        } else {
-            LED_STRIP[i] = black;
+        if (left_max <= i) {
+            LED_STRIP[i] = CRGB::Black;
         }
-    }
-
-    for (int i = 0; i < NUM_LEDS - 2; i++) {
-        LED_STRIP[i + 1] = LED_STRIP[i].lerp8(LED_STRIP[i + 1], 127);
     }
 
     for (int i = NUM_LEDS - 1; i >= channel_count; i--) {
-        if (right_max < i) {
-            LED_STRIP[i] = color;
-
-        } else {
-            LED_STRIP[i] = black;
+        if (right_max >= i) {
+            LED_STRIP[i] = CRGB::Black;
         }
     }
 
-    for (int i = NUM_LEDS - 1; i > channel_count; i--) {
-        LED_STRIP[i - 1] = LED_STRIP[i].lerp8(LED_STRIP[i - 1], 127);
-    }
+    blur1d(LED_STRIP, NUM_LEDS, 128);
 
+    analogWrite(LEFT_LED_PIN, mapf(channels[0], 0, max_recorded, 0, 255));
+    analogWrite(RIGHT_LED_PIN, mapf(channels[1], 0, max_recorded, 0, 255));
 
     FastLED.delay(10);
 }
